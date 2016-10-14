7afa665872cd948daa6e212a55f34f2a455b6166
themsoum7 <mariksoum@gmail.com>
2016-10-15 00:12:51 +0300

    second commit

---
+++ src/flowerOne.java
@@ -0,0 +1,13 @@
+/**
+ * Created by r0bert on 14.10.2016.
+ */
+public class flowerOne extends Flower {
+    public flowerOne(double stemLength, boolean Freshness) {
+
+        this.stemLength = stemLength;
+        this.Price(5);
+        this.Freshness(false);
+        this.isFresh = isFresh;
+
+    }
+}
---
+++ src/flowerTwo.java
@@ -0,0 +1,13 @@
+/**
+ * Created by r0bert on 14.10.2016.
+ */
+public class flowerTwo extends Flower{
+    public flowerTwo(double stemLength, boolean Freshness) {
+
+        this.stemLength = stemLength;
+        this.Price(10);
+        this.Freshness(true);
+        this.isFresh = isFresh;
+
+    }
+}
